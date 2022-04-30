d2caed89983c9492c2217b97ff6cf817085f9d20
Salim A. JOLY <el.salim.salim@gmail.com>
2022-04-30 14:16:34 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Skin from "./Skin";
 import Home from "./pages/Home";
@@ -8,13 +8,25 @@ import "./App.css";
 
 export default function App() {
 
+  const [isFetchingData, setIsFetchingData] = useState(false);
+
+
+  useEffect(() => {
+      console.log("Page loading");
+  },[]);
+
+  console.log("test");
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={ <Skin><Home/></Skin> } />
-        <Route path="/profile" element={ <Skin><Profile/></Skin> } />
-        <Route path="/config" element={ <Skin><Config/></Skin> } />
-      </Routes>
+
+      { isFetchingData 
+        ? <Routes>
+            <Route path="/" element={ <Skin><Home/></Skin> } />
+            <Route path="/profile" element={ <Skin><Profile/></Skin> } />
+            <Route path="/config" element={ <Skin><Config/></Skin> } />
+          </Routes>
+        : <div><h1>Loading...</h1></div>
+      }
     </div>
   );
 }
